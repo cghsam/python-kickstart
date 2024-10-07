180f5622d2e84e4dd53d3591a8f529308b70fa22
cghsam <fbmallusk@gmail.com>
2024-10-07 17:57:34 +0100

    Just learning

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("### Welcome to MyMusclePal! ###")
+weight = input("What is your weight in kilograms (kg)? Type the number and press Enter: ")
+protein = int(weight) * 2
+print("Based on your weight of ", str(weight) + "kg, you need ", str(protein) + "kg of protein." )
